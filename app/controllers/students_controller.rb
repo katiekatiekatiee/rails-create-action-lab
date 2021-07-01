5512feb1911fbb7786ca3de80eebad4dcc6d8aa6
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-06-30 23:02:04 -0400

    commit tree f2918e9c8c90022b12544e5d75fe3285b5ebeabc

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,9 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+    #byebug
     @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
+    
     @student.save
     #@student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to student_path(@student)
